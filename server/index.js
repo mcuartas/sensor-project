9f2db06e594260b72e00a3ba3ed14e7b2fbbe31a
Miguel Cuartas <miguel.cuartas@gmail.com>
2018-01-02 14:07:44 +0100

    sql error solved

--- server/index.js
+++ server/index.js
@@ -8,13 +8,13 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/temperature', function(req, res) {
 	res.json({
-		value: getCachedSensorReadings.getTemperature().toFixed(1)
+		value: (getCachedSensorReadings.getTemperature() || 0.0).toFixed(1)
 	})
 })
 
 app.get('/humidity', function(req, res) {
 	res.json({
-		value: getCachedSensorReadings.getHumidity().toFixed(1)
+		value: (getCachedSensorReadings.getHumidity() || 0.0).toFixed(1)
 	})
 });
 
